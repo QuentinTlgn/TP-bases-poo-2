47673b4dde0cf15d9b5f7a0688a185198cd66588
QuentinTlgn <tauleigq@3il.fr>
2023-11-23 16:58:31 +0100

    Création du constructor et vérification valeurs

--- segment/Segment.java
+++ segment/Segment.java
@@ -1,7 +1,23 @@
 package segment;
 
 public class Segment {
-	private int extr1;
-	private int extr2;
+	private float extr1;
+	private float extr2;
+	
+	public Segment(int extr1, int extr2) {
+		this.extr1 = extr1;
+		this.extr2 = extr2;
+		verifierValeur(this.extr1, this.extr2);
+	}
+	
+	private void verifierValeur(float extr1, float extr2) {
+		if(extr1 > extr2) {
+			throw new IllegalArgumentException("L'extremité 1 est supérieure à l'extrémité 2");
+		}
+	}
+	
+	
+	
+	
 	
 }
