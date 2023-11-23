3270859e05d5ccb75d7f4eeb132431037b183482
QuentinTlgn <tauleigq@3il.fr>
2023-11-23 15:56:09 +0100

    Ajout fonctions + Constructor

--- ModelisationSimple/Chien.java
+++ ModelisationSimple/Chien.java
@@ -5,4 +5,18 @@ public class Chien {
 	private Integer age = 8;
 	private String couleur = "Marron";
 	
+	public void aboyer() {
+		System.out.println("Ouaf !");
+	}
+	
+	public void dormir() {
+		System.out.println("Zzzzzzz");
+	}
+
+	public Chien(String race, Integer age, String couleur) {
+		super();
+		this.race = race;
+		this.age = age;
+		this.couleur = couleur;
+	}
 }
